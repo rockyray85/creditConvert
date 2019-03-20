053ecff72d828ba8f91a7b4d17fe47463d611433
Rocky <none@email.com>
2019-03-20 09:34:29 +0200

    initial commit

---
+++ mainConverter.py
@@ -0,0 +1,9 @@
+import #XLS reader library
+import #mongoDB library
+import trxParse
+
+def main(rawXls):
+
+
+if __name__ == "__main__":
+    main()
---
+++ trxParse.py
@@ -0,0 +1,12 @@
+def parseParams (cardType, cardNum, trxLocation, trx):
+    if cardType = 'isracard-mastercard':
+        #! 
+        #!
+    elif cardType = 'leumicard-visa':
+        #!
+    elif cardType = 'isracard-amex':
+        #!
+    elif cardType = 'isracard-amex-lifestyle':
+        #!
+
+
